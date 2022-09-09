5ba9413842047d7693048a804ea3cca3345db0b5
camipozas <cpozasg1103@gmail.com>
2022-09-08 22:33:38 -0400

    asynchronous function to call functions and get values

--- src/index.js
+++ src/index.js
@@ -7,16 +7,22 @@ const getEUR = require("./indicators/getEUR");
 const getUTM = require("./indicators/getUTM");
 const getIPC = require("./indicators/getIPC");
 
+/**
+ * It fetches data from the Central Bank of Chile's API, merges it into a single object, and returns it
+ */
 const main = async () => {
     const today = dayjs().format("YYYY/MM/[dias]/DD");
     const thisMonth = dayjs().format("YYYY/MM");
     const lastMonth = dayjs().subtract(1, "month").format("YYYY/MM");
 
-    const ufData = await getUF(today);
-    const usdData = await getUSD(today);
-    const eurData = await getEUR(today);
-    const utmData = await getUTM(thisMonth);
-    const ipcData = await getIPC(lastMonth);
+    const [ufData, usdData, eurData, utmData, ipcData] = await Promise.all([
+        getUF(today),
+        getUSD(today),
+        getEUR(today),
+        getUTM(thisMonth),
+        getIPC(lastMonth),
+    ]);
+
     const mergedData = {
         ...ufData,
         ...usdData,
@@ -24,7 +30,6 @@ const main = async () => {
         ...utmData,
         ...ipcData,
     };
-    console.log(mergedData);
 };
 
 main();
