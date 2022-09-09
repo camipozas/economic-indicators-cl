3c80e81fa4aa4b4ef237965ad6412529300470c6
camipozas <cpozasg1103@gmail.com>
2022-09-08 22:32:21 -0400

    delete file

--- src/date.js
+++
@@ -1,7 +0,0 @@
-const dayjs = require("dayjs");
-
-/* Getting the current date and formatting it to the format that the API requires. */
-const today = dayjs().format("YYYY/MM/[dias]/DD");
-const thisMonth = dayjs().format("YYYY/MM");
-
-module.exports = today;
