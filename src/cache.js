3599b63ee962512a18bafe8f2cd28c2aaa390391
Camila Pozas <cpozasg1103@gmail.com>
2022-09-25 01:03:41 -0400

    update cache, redis in stand by

--- src/cache.js
+++ src/cache.js
@@ -1,13 +1,18 @@
 const cacheManager = require("cache-manager");
-const redisStore = require("cache-manager-redis-store");
+// const redisStore = require("cache-manager-redis-store");
 
-const redisCache = cacheManager.caching({
-    store: redisStore,
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    auth_pass: process.env.REDIS_PASS,
-    db: 0,
+// const redisCache = cacheManager.caching({
+//     store: redisStore,
+//     host: process.env.REDIS_HOST,
+//     port: process.env.REDIS_PORT,
+//     auth_pass: process.env.REDIS_PASS,
+//     db: 0,
+//     ttl: 60 * 60, // 1 hour
+// });
+
+const memoryCache = cacheManager.caching({
+    store: "memory",
     ttl: 60 * 60, // 1 hour
 });
 
-module.exports = redisCache;
+module.exports = memoryCache;
